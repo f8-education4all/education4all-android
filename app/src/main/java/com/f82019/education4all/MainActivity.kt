9d5b1b650f156e176c30da4e9209f09ae7dcff17
Jeongah Shin <jeongah.h.shin@gmail.com>
2019-04-29 09:31:44 +0900

    Camera2 Basic structure added

---
+++ app/src/main/java/com/f82019/education4all/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.f82019.education4all
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import com.f82019.education4all.camera.Camera2BasicFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (null == savedInstanceState) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit()
+        }
+    }
+}
